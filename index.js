234d948bd554f573fd1ffc725ee39ccc8228b291
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-09 17:47:34 +0530

    Create a in-memory hospital server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 
+const app = express()   // creating the clinic
+
 const users = [{
     name: "Stark",
     kidneys: [{
@@ -7,7 +9,60 @@ const users = [{
     }]
 }]
 
-const app = express()  // creating the clinic
+app.use(express.json())
 
+
+app.get("/", function(req, res){
+    const johnKidneys = users[0].kidneys;
+    const numberofKidneys = johnKidneys.length
+    let numberofHealthyKidneys = 0
+    for(let i = 0; i < johnKidneys.length; i++)
+    {
+        if(johnKidneys[i].healthy){
+            numberofHealthyKidneys = numberofHealthyKidneys + 1;
+        }
+    }
+    const unhealthyKidneys = numberofKidneys - numberofHealthyKidneys;
+
+    res.json({
+        numberofKidneys,
+        numberofHealthyKidneys,
+        unhealthyKidneys
+    })
+})
+
+app.post("/", function(req, res){
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy : isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
+
+app.put("/", function(req, res){
+    for(let i = 0; i < users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({});
+})
+
+
+// removing all the unhealthy kidneys
+app.delete("/", function(req, res){
+    const newKidneys = [];
+    for(let i = 0; i < users[0].kidneys.length; i++){
+        if(users[0].kidneys[i].healthy){
+            newKidneys.push({
+                healthy: true
+            })
+        }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({
+        msg : "Done !"
+    })
+})
 
 app.listen(3000)   // docter actually taking the room
